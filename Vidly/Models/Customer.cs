00bced09be20079b9c4280c1301ced9374f5d24b
haugan <hauganmarius@gmail.com>
2017-06-12 16:55:34 +0200

    Seeding database

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -5,5 +5,8 @@
         public int Id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
