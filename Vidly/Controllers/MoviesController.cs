2320b06edac3facdcf76d99de8705f3b837d0df5
haugan <hauganmarius@gmail.com>
2017-06-12 11:23:41 +0200

    Initial commit
    Basic web application template with new Bootstrap theme and a demo data
    model.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: movies/random
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Memento"
+            };
+
+            return View(movie);
+        }
+    }
+}
