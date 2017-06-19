98ea1e05e3ada549a1ba4dfbe22689a16d309e23
haugan <hauganmarius@gmail.com>
2017-06-19 13:57:13 +0200

    Code cleanup

--- Vidly/ViewModels/MovieFormViewModel.cs
+++ Vidly/ViewModels/MovieFormViewModel.cs
@@ -5,43 +5,9 @@ using Vidly.Models;
 
 namespace Vidly.ViewModels
 {
+    // DEMONSTRATING A "PURE" VIEW MODEL
     public class MovieFormViewModel
     {
-        // GENRE LIST
-        public IEnumerable<Genre> Genres { get; set; }
-
-        // MOVIE ID
-        public int? Id { get; set; } // Nullable type initializes to null (default: 0)
-
-        // MOVIE TITLE
-        [StringLength(255)]
-        [Required(ErrorMessage = "Title is required.")]
-        public string Title { get; set; }
-
-        // MOVIE RELEASE DATE
-        [Display(Name = "Release date")]
-        [Required(ErrorMessage = "Release date is required.")]
-        public DateTime? ReleaseDate { get; set; } // Nullable type initializes to null (default: 01-01-2001)
-
-        // MOVIE STOCK AMOUNT
-        [Display(Name = "Stock amount")]
-        [Range(1, 20, ErrorMessage = "Stock amount must be between 1 and 20.")]
-        [Required(ErrorMessage = "Stock amount is required.")]
-        public int? Stock { get; set; } // Nullable type initializes to null (default: 0)
-
-        // GENRE ID
-        [Required(ErrorMessage = "Genre is required.")]
-        public byte? GenreId { get; set; } // Nullable type initializes to null (default: 0)
-
-        // DYNAMIC PAGE HEADER
-        public string PageHeader
-        {
-            get
-            {
-                return (Id != 0) ? "Edit movie" : "New movie";
-            }
-        }
-
         // DEFAULT CONSTRUCTOR FOR WHEN CREATING NEW MOVIE
         public MovieFormViewModel()
         {
@@ -57,5 +23,32 @@ namespace Vidly.ViewModels
             Stock = movie.Stock;
             GenreId = movie.GenreId;
         }
+
+        // DYNAMIC PAGE HEADER
+        public string PageHeader
+        {
+            get
+            {
+                return (Id != 0) ? "Edit movie" : "New movie";
+            }
+        }
+
+        // MOVIE ID
+        public int? Id { get; set; } // Nullable type initializes to null (default: 0)
+
+        // MOVIE TITLE
+        public string Title { get; set; }
+
+        // MOVIE RELEASE DATE
+        public DateTime? ReleaseDate { get; set; } // Nullable type initializes to null (default: 01-01-2001)
+
+        // MOVIE STOCK AMOUNT
+        public int? Stock { get; set; } // Nullable type initializes to null (default: 0)
+
+        // GENRE ID
+        public byte? GenreId { get; set; } // Nullable type initializes to null (default: 0)
+
+        // GENRE LIST
+        public IEnumerable<Genre> Genres { get; set; }
     }
 }
