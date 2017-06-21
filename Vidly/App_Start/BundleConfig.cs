f96ebb7d0308a6f607f5ce6ea344d703a342d257
haugan <hauganmarius@gmail.com>
2017-06-21 16:50:40 +0200

    Customer delete links, confirm window, and table upgrade (pagination, sorting, searching)
    jQuery, Bootbox, & DataTables

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -4,20 +4,33 @@ namespace Vidly
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        // Bundling: https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+            // JAVASCRIPT LIBRARIES
+            bundles.Add(new ScriptBundle("~/bundles/libs").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/bootbox.js",
+                "~/Scripts/respond.js",
+                "~/Scripts/DataTables/media/js/jquery.dataTables.js",
+                "~/Scripts/DataTables/media/js/dataTables.bootstrap.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
+            // VALIDATION
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/jquery.validate*"));
 
+            // MODERNIZR
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap-flatly.css", "~/Content/site.css"));
+            // CSS STYLING
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap-flatly.css",
+                "~/Content/DataTables/media/css/dataTables.bootstrap.css",
+                "~/Content/site.css"));
         }
     }
 }
